4033c03c06402cc0d08743d1cab8eaf0fe888ead
Jean Villete <jean.villete@gmail.com>
2019-06-01 12:30:40 -0300

    arranging spring profiles over project components

--- src/test/java/com/clockinclockout/restopenshift/user/UserControllerTest.java
+++ src/test/java/com/clockinclockout/restopenshift/user/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@ActiveProfiles( "dev-local" )
+@ActiveProfiles( "dev-local" )
 @RunWith( SpringRunner.class )
 @SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
 @AutoConfigureMockMvc
